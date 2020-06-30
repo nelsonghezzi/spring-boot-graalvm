f5e9f7e79c7b8845e6064510470aaabedcd42ed6
Nelson Germán Ghezzi <nelsonghezzi@users.noreply.github.com>
2020-06-30 00:46:24 -0300

    Initial version based on first post of the series

---
+++ src/main/java/com/example/demo/handler/HelloRouter.java
@@ -0,0 +1,21 @@
+package com.example.demo.handler;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Component
+public class HelloRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(HelloHandler helloHandler) {
+        return RouterFunctions.route(
+            RequestPredicates.GET("/hello").and(RequestPredicates.accept(MediaType.TEXT_PLAIN)),
+            serverRequest -> helloHandler.hello(serverRequest)
+        );
+    }
+}
